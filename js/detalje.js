4ac2e2b4d937ad92292ee47f18671181b10bbc50
ambe0001 <ambe0001@stud.kea.dk>
2025-02-18 19:09:04 +0100

    detaljedone

--- js/detalje.js
+++ js/detalje.js
@@ -17,18 +17,20 @@ function showProduct(data) {
   productContainer.innerHTML = ` <img class="${data.stock === 0 && "udsolgt_f"} marginleft produktfoto" src="${data.images[1]}" alt="">
 
             <div>
-<h1>${data.brandname}</h1>
+<h1 class="titledetalje">${data.title}</h1>
+
                <div class="rabatprodukt ${data.discountPercentage && "rabatprodukt_fr"} ">
                     <p>${data.discountPercentage}%</p>
                 </div>
 
-                <div class="udsolgtprodukt ${data.soldout && "udsolgtprodukt_fr"}">
+                <div class="udsolgtprodukt ${data.stock === 0 && "udsolgtprodukt_fr"}">
                     <p>Udsolgt</p>
                 </div>
+
                 <h2 class="produktinfo">Produkt information</h2>
                 <div>
-                    <h3 class="specifik marginleft">Modelname</h3>
-                    <p class="marginleft">${data.productdisplayname}</p>
+                    <h3 class="specifik marginleft">Brand</h3>
+                    <p class="marginleft">${data.brand}</p>
                 </div>
                 <div>
                     <h3 class="specifik marginleft">Inventory number</h3>
@@ -36,10 +38,13 @@ function showProduct(data) {
                 </div>     
           
             <div class="flex kurv">
-                <h3>${data.productdisplayname}</h3>
-                <h4>${data.articletype}|${data.brandname}</h4>
-                <p>${data.price},-</p>
+                <h3>Pris før udsalg</h3>
+                 <p class="bold"> <b>${data.price},- </b></p>
+        
+
                 <div>
+
+                    <p>${data.warrantyInformation} |  ${data.shippingInformation}</p>
                     <p class="tilføjkurv">Tilføj kurv</p>
                 </div>
             </div>
